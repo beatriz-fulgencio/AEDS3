f5da11c7faaa0c66ac8ae4226dfedfbfd5e2927c
biapessoab <104511205+biapessoab@users.noreply.github.com>
2023-02-27 19:59:58 -0300

    lendo o csv

--- Movie.java
+++ Movie.java
@@ -6,27 +6,27 @@ public class Movie {
 
     static SimpleDateFormat format = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH);
 
-    private long pos;
-   // private String movieId;
+    // private long pos;
+    private String movieId;
     private String title;
-    private ArrayList<String> genres;
+    private String genres;
     private int duration;
     private String contentType;
     private Date dateAdded;
 
     public Movie() {
-        this.pos = 0;
-        //this.movieId = "";
+        // this.pos = 0;
+        this.movieId = "";
         this.title = "";
-        this.genres = new ArrayList<String>();
+        this.genres = null;
         this.duration = 0;
         this.contentType = "";
         this.dateAdded = null;
     }
 
-    public Movie(String movieId, String title, ArrayList<String> genres, int duration, String contentType,
+    public Movie(String movieId, String title, String genres, int duration, String contentType,
             Date dateAdded) {
-       // this.movieId = movieId;
+        this.movieId = movieId;
         this.title = title;
         this.genres = genres;
         this.duration = duration;
@@ -35,21 +35,22 @@ public class Movie {
     }
 
     // Gets e Sets
-    public void set_pos(long p){
-        this.pos = p;
-    }
 
-    public long get_pos(){
-        return this.pos;
-    }
-
-    // public void set_movieId(String movieId) {
-    //     this.movieId = movieId;
+    // public void set_pos(long p){
+    // this.pos = p;
     // }
 
-    // public String get_movieId() {
-    //     return movieId;
+    // public long get_pos(){
+    // return this.pos;
     // }
+
+    public void set_movieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String get_movieId() {
+        return movieId;
+    }
 
     public void set_title(String title) {
         this.title = title;
@@ -59,11 +60,11 @@ public class Movie {
         return title;
     }
 
-    public void set_genres(ArrayList<String> genres) {
+    public void set_genres(String genres) {
         this.genres = genres;
     }
 
-    public ArrayList<String> get_genres() {
+    public String get_genres() {
         return genres;
     }
 
@@ -91,164 +92,88 @@ public class Movie {
         return dateAdded;
     }
 
-    public void read(String line) {
-        int index = 0, atr_index = 0;
+    private String[] splitLine(String str) {
+        String[] atributos = new String[6];
+        int cont = 0;
+        String aux = "";
 
-        // ---------------------------------- //
-        // Find "ID"
-        while (true) {
-            index++;
-            if (line.charAt(index) == ';') {
-                //this.movieId = line.substring(atr_index, index);
-                atr_index = ++index;
-                break;
+        for (int i = 0; i < str.length(); i++) {
+            if (i != str.length() - 1) {
+                if (str.charAt(i) != ';') {
+                    aux += str.charAt(i);
+                } else {
+                    atributos[cont++] = aux;
+                    aux = "";
+                }
+            } else {
+                aux += str.charAt(i);
+                atributos[cont++] = aux;
+                aux = "";
             }
         }
-        // ---------------------------------- //
-        // Find "title"
-        if (line.charAt(atr_index) != ';') {
-            if (line.charAt(atr_index) == '#')
-                atr_index++;
-
-            while (true) {
-                index++;
-                if (line.charAt(index) == ';') {
-                    this.title = line.substring(atr_index, index);
-                    index++;
-                    atr_index = index;
-                    break;
-                }
-            }
-        } else
-            atr_index = ++index;
-        // ---------------------------------- //
-        // Find "genres"
-        if (line.charAt(index) != ';') {
-            while (true) {
-                if(line.charAt(index) == ';'){
-                    index++;
-                    atr_index = index;
-                    break;
-                }else{
-                int wordStart = index++;
-                while (true) {
-                    index++;
-                    if (line.charAt(index) == ',' ||line.charAt(index) == ';') {
-                        this.genres.add(line.substring(wordStart, index));
-                        break;
-                    } 
-                }
-            }
-
-            }
-        } else {
-            atr_index = ++index;
-        }
-        // ---------------------------------- //
-        // Find "duration"
-        if (line.charAt(index) != ';') {
-            while (true) {
-                index++;
-                if (line.charAt(index) == ';') {
-                    this.duration = Integer.parseInt(line.substring(atr_index, index));
-                    atr_index = ++index;
-                    break;
-                }
-            }
-        } else {
-            atr_index = ++index;
-        }
-        // ---------------------------------- //
-        // Find "contentType"
-        if (line.charAt(index) != ';') {
-            while (true) {
-                index++;
-                if (line.charAt(index) == ';') {
-                    this.contentType = line.substring(atr_index, index);
-                    atr_index = ++index;
-                    break;
-                }
-            }
-        } else {
-            atr_index = ++index;
-        }
-        // ---------------------------------- //
-        // Find "Date"
-        if (line.charAt(atr_index) != ';') {
-            while (index != line.length()) {
-                index++;
-            }
-            try {
-                this.dateAdded = format.parse(line.substring(atr_index, index));
-            } catch (java.text.ParseException e) {
-                e.printStackTrace();
-            }
-
-        } else
-            atr_index = ++index;
+        return atributos;
     }
 
+    public Date convertToDate(String strData) throws Exception {
+        Date date = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("MMMM dd, yyyy", Locale.US);
+        date = sdf.parse(strData);
+        return date;
+    }
 
-    public byte[] toByteArray() throws IOException{
+    public void read(String line) throws Exception {
+        String[] atributos = splitLine(line);
+
+        set_movieId(atributos[0]);
+        set_title(atributos[1]);
+        set_genres(atributos[2]);
+        set_duration(Integer.parseInt(atributos[3]));
+        set_contentType(atributos[4]);
+        set_dateAdded(convertToDate(atributos[5]));
+
+        // System.out.println("\nID: " + movieId +
+        // "\nTitle: " + title +
+        // "\nGenres: " + genres +
+        // "\nDuration: " + duration +
+        // "\nContent Type: " + contentType +
+        // "\nDate Added: " + format.format(dateAdded));
+    }
+
+    public byte[] toByteArray() throws IOException {
+
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
 
-
-        //dos.writeByte(0); //lapide
-
-        dos.writeLong(pos); 
-        
+        dos.writeUTF(movieId);
         dos.writeUTF(title);
-
-        dos.writeShort(genres.size());
-        for(String s:genres){
-            dos.writeUTF(s);
-        }
-
-        dos.writeShort(duration);
-
-        byte[] s = contentType.getBytes();
-        dos.write(s, 0, 5);
-
-        long mills = dateAdded.getTime();
-
-        dos.writeLong(mills);
-
+        dos.writeUTF(genres);
+        dos.writeInt(duration);
+        dos.writeUTF(contentType);
+        dos.writeLong(dateAdded.getTime());
 
         return baos.toByteArray();
     }
 
-    public void fromByteArray(byte[] ba) throws IOException{
+    public void fromByteArray(byte ba[]) throws IOException {
+
         ByteArrayInputStream bais = new ByteArrayInputStream(ba);
         DataInputStream dis = new DataInputStream(bais);
 
-        //bais.skip(1); //pular lapide
-
-        this.pos = dis.readLong();
-
-        this.title = dis.readUTF();
-
-        for(short i = 0; i< dis.readShort(); i++){
-            genres.add(dis.readUTF());
-        }
-
-        this.duration = dis.readShort();
-
-        byte[] s = dis.readNBytes(5);
-        this.contentType = s.toString();
-
-    
+        movieId = dis.readUTF();
+        title = dis.readUTF();
+        genres = dis.readUTF();
+        duration = dis.readInt();
+        contentType = dis.readUTF();
         long mills = dis.readLong();
-        this.dateAdded.setTime(mills);
+
     }
 
-    public String toString(){
-        return "\n ID:" + pos +
-                "\n Title:" + title +
-                "\n Genres:" + genres.toString() +
-                "\n Duration:" + duration +
-                "\n Content Type:" + contentType +
-                "\nDate Added:" + format.format(dateAdded);
-
+    public String toString() {
+        return "\nID: " + movieId +
+                "\nTitle: " + title +
+                "\nGenres: " + genres +
+                "\nDuration: " + duration +
+                "\nContent Type: " + contentType +
+                "\nDate Added: " + format.format(dateAdded);
     }
 }

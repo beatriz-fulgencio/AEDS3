fda5cd6c351cd615761a95888f569b949bf71a21
biapessoab <104511205+biapessoab@users.noreply.github.com>
2023-02-27 19:59:18 -0300

    lendo o csv

--- exTP.java
+++ exTP.java
@@ -2,22 +2,18 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 
-public class ExTP {
+public class exTP {
 
-    public static String[] read(String[] strs, int maxRead) throws FileNotFoundException {
+    public static String[] readCSV(String[] strs, int maxRead) throws FileNotFoundException {
         File file = new File("netflix.csv");
-        Scanner sc = null;
+        Scanner sc = new Scanner(file);
         int place = 0;
-
-        try {
-            sc = new Scanner(file);
-            while (place<maxRead) {
-                strs[place] = sc.nextLine();
-                place++;
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        
+        while(sc.hasNext()) {
+            
+            place++;
         }
+
         sc.close();
 
         return strs;
@@ -25,69 +21,76 @@ public class ExTP {
 
     public static void main(String[] args) throws Exception {
 
-        int n =  10/*3900*/;
-        String[] movies = new String[n];
-        Movie[] movieObjects = new Movie[n];
-        movies = read(movies, n);
+        int n =  3800;
 
-        int countMovies = 0;
-        //for (int i = 0; i < n; i++) {
-            for (int j = 0; j < movies.length; j++) {
-                movieObjects[countMovies] = new Movie();
-                movieObjects[countMovies].read(movies[j]);
-                movieObjects[countMovies].set_pos(j);
-                countMovies++;
-                //j = movies.length;
-            }
-        //}
+        Scanner sc = new Scanner(new File("netflix.csv"));
+        Movie movie = new Movie();
 
+        while(sc.hasNext()) {
+            movie.read(sc.nextLine());
+            movie.toString();
+            // System.out.println(movie.toString());
+        }
 
-        byte[] ba;
         long  pos = 0;
         long lastPos = 0;
         int len;
+
+        Movie m1= new Movie("e2ef4e91-fb25-42ab-b485-be8e3b23dedb", "#Alive", "Horror Movies, International Movies, Thrillers", 99, "Movie", );
+        Movie m2= new Movie("b01b73b7-81f6-47a7-86d8-acb63080d525", "#AnneFrank - Parallel Stories", "Documentaries, International Movies", 95, "Movie", July 1, 2020);
+        Movie m3= new Movie("7f2d4170-bab8-4d75-adc2-197f7124c070", "#cats_the_mewvie", "Documentaries, International Movies", 90, "Movie", February 5, 2020);
+
         Movie m_temp = new Movie();
+        byte[] ba;
+        long pos0,pos1,pos2;
+
         try {
 
-            RandomAccessFile arq = new RandomAccessFile("movies.db", "rw");
-            //arq.skipBytes(4);
-            for(Movie m: movieObjects){
-                //lastPos = m.get_pos();
-                ba = m.toByteArray();
-                arq.writeBoolean(false);
-                //pos = arq.getFilePointer();
-                arq.writeInt(ba.length);
-                arq.write(ba);
-            }
-            arq.seek(0);
-            //arq.writeLong(lastPos);
-            
-            // arq.seek(pos);
-            // lastPos = arq.readLong();
-            // arq.seek(0);
-            // arq.writeLong(lastPos);
+            RandomAccessFile arq = new RandomAccessFile("../dados/jogadores.db", "rw");
 
+            pos0=arq.getFilePointer();
+            System.out.println("Registro iniciado na posição: "+pos0);
+            ba = m1.toByteArray();
+            arq.writeInt(ba.length); //Tamano do registro em bytes
+            arq.write(ba);
+            
+            pos1=arq.getFilePointer();
+            System.out.println("Registro iniciado na posição: "+pos1);
+            ba = m2.toByteArray();
+            arq.writeInt(ba.length); //Tamano do registro em bytes
+            arq.write(ba);
 
-            arq.seek(0);
-            //arq.skipBytes(4);
+            pos2=arq.getFilePointer();
+            System.out.println("Registro iniciado na posição: "+pos2);
+            ba = m3.toByteArray();
+            arq.writeInt(ba.length); //Tamano do registro em bytes
+            arq.write(ba);
             
-            boolean lapide;
-            
-            lapide = arq.readBoolean();
-            if(!lapide){
-                len = arq.readInt();
-                ba = new byte[len];
-                arq.read(ba);
-                m_temp.fromByteArray(ba);
-                System.out.println(m_temp);
-            }
-                arq.close();
-            
+            //Lendo por ponteiro de trás para frente
+            arq.seek(pos2);
+            len = arq.readInt();
+            ba = new byte[len];
+            arq.read(ba);
+            m_temp.fromByteArray(ba);
+            System.out.println(m_temp);
 
+            arq.seek(pos1);
+            len = arq.readInt();
+            ba = new byte[len];
+            arq.read(ba);
+            m_temp.fromByteArray(ba);
+            System.out.println(m_temp);
+
+            arq.seek(pos0);
+            len = arq.readInt();
+            ba = new byte[len];
+            arq.read(ba);
+            m_temp.fromByteArray(ba);
+            System.out.println(m_temp);
+            
         } catch (Exception e) {
-            System.out.println(e.getMessage()); // TODO: handle exception
+            System.out.println(e.getMessage());
         }
-
     }
 
 }

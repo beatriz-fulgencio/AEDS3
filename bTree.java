e7db2c1384bda258cf20903920df01569a7d958f
biapessoab <104511205+biapessoab@users.noreply.github.com>
2023-04-05 09:12:22 -0300

    Testando terminal

---
+++ bTree.java
@@ -0,0 +1,6 @@
+public class bTree {
+    
+    int[] node = new int[7]; // ordem 8 -> no máximo 7 filhos
+
+
+}
--- exTP.java
+++ exTP.java
@@ -29,9 +29,9 @@ public class exTP {
     }
 
     public static void main(String[] args) throws Exception {
-        Crud byteFileRandler = new Crud("movies.db");
+        // Crud byteFileRandler = new Crud("movies.db");
 
-        read(byteFileRandler);
+        // read(byteFileRandler);
 
         //  byteFileRandler.clear();
         // byteFileRandler.read("0000");
@@ -40,11 +40,11 @@ public class exTP {
 
         // byteFileRandler.update("0000");
 
-        Sort fileSort = new Sort("movies.db");
+        // Sort fileSort = new Sort("movies.db");
 
         // fileSort.clear();
 
-        fileSort.intercalacaoBalanceadaComum();
+        // fileSort.intercalacaoBalanceadaComum();
 
         // byteFileRandler.create();
         // byteFileRandler.delete("0099");

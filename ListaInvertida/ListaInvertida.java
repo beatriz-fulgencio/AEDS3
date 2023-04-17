fd18b0021b9536d2b62ce2ce12cd875c6897ab62
biapessoab <104511205+biapessoab@users.noreply.github.com>
2023-04-17 11:30:04 -0300

    starting inverted index

--- ListaInvertida/ListaInvertida.java
+++ ListaInvertida/ListaInvertida.java
@@ -10,35 +10,5 @@ public class ListaInvertida {
         fileReader = new RandomAccessFile(file, "rw");
     }
 
-    int i, j;
-
-    Matrix m = new Matrix();
-
-    // get the duration
-    for(i=0;i<20;i++) {
-        for (j = 0; j < 20; j++) {
-            if (movies[i].get_duration != movies[j].get_duration && i != j) {
-                m.durationArray.push(movies[i].get_duration);
-            }
-        }
-    }
-
-    // get the genres
-    for(i=0;i<20;i++) {
-        for (int j = 0; j < 20; j++) {
-            if (movies[i].get_genres != movies[j].get_genres && i != j) {
-                m.genresArray.push(movies[i].get_genres);
-            }
-        }
-    }
-
-    // creating the matrix
-
-    for(i=0;i<durationArray.length;i++) {
-        for (j = 0; j < movies.length; j++) {
-            if (movies[j].get_duration == durationArray[i]) {
-
-            }
-        }
-    }
+    
 }

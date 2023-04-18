ed37441fce935de918375d49c172ece2c37049d7
biapessoab <104511205+biapessoab@users.noreply.github.com>
2023-04-17 21:31:24 -0300

    constructing the element

---
+++ ListaInvertida/Element.java
@@ -0,0 +1,52 @@
+import java.io.*;
+
+public class Element {
+
+    private long address; // the buckets beggining address
+    private int id; // movie id
+
+    File file;
+    RandomAccessFile fileReader;
+
+    // constructor
+    public Element() {
+        this.address = 0;
+        this.id = 0;
+    }
+
+    public Element(long address, int id) throws FileNotFoundException {
+        this.address = address;
+        this.id = id;
+        fileReader = new RandomAccessFile(file, "rw");
+    }
+
+    // gets and sets
+    public long get_address() {
+        return address;
+    }
+
+    public void set_address(long address) {
+        this.address = address;
+    }
+
+    public int get_id() {
+        return id;
+    }
+
+    public void set_id(int id) {
+        this.id = id;
+    }
+
+    // file reader and writer
+    public void WriteFile() throws IOException {
+        fileReader.seek(address); // seeks the buckets addres in the hash file
+        fileReader.writeInt(id); // writes movie id
+        fileReader.writeLong(address); // writes address
+    }
+
+    public void readFile(long pos) throws IOException {
+        fileReader.seek(pos); // seeks the address provided
+        id = fileReader.readInt(); // reads the id
+        address = fileReader.readLong(); // reads address
+    }
+}
--- ListaInvertida/ListaInvertida.java
+++ ListaInvertida/ListaInvertida.java
@@ -9,6 +9,4 @@ public class ListaInvertida {
         this.file = new File(F); // creates the "file" file
         fileReader = new RandomAccessFile(file, "rw");
     }
-
-    
 }
